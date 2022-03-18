ec2e608991acc7f9995b36cb51c7ac56843c08ee
jairocomino <101825315+jairocomino@users.noreply.github.com>
2022-03-18 08:56:03 +0100

    Create Animal.java

---
+++ GitEntornosNetbeans/src/Animal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public  abstract class Animal {
+    private boolean despierto;
+    
+    public void dormir(){
+        this.despierto=false;
+    
+    }
+    public void despertar(){
+    this.despierto=true;
+    }
+    public boolean estadespierto(){
+        return despierto;
+    }
+}
